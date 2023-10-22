648a409afdf8b9a1670f4b079169f17ed982db5d
Travis Bumgarner <travis.bumgarner@gmail.com>
2023-10-22 13:26:20 -0600

    clear

--- gcode2dplotterart-website/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/Users/travisbumgarner/Programming/gcode2dplotterart/gcode2dplotterart-website/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/travisbumgarner/Programming/gcode2dplotterart/gcode2dplotterart-website/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/travisbumgarner/Programming/gcode2dplotterart/gcode2dplotterart-website/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/Users/travisbumgarner/Programming/gcode2dplotterart/gcode2dplotterart-website/src/css/custom.css'),
-];
