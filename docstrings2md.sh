00f6be0041703ca1b86dfc99b87e99c14afb4397
Travis Bumgarner <travis.bumgarner@gmail.com>
2023-10-25 17:49:02 -0600

    Implemented docs in website

--- docstrings2md.sh
+++ docstrings2md.sh
@@ -1,2 +1,2 @@
-pydoc-markdown -m gcode2dplotterart/Plotter -I $(pwd) > README_PLOTTER.md
-pydoc-markdown -m gcode2dplotterart/Layer -I $(pwd) > README_LAYER.md
+pydoc-markdown -m gcode2dplotterart/Plotter -I $(pwd) > ./gcode2dplotterart-website/docs/api/Plotter.mdx
+pydoc-markdown -m gcode2dplotterart/Layer -I $(pwd) > ./gcode2dplotterart-website/docs/api/Layer.mdx
