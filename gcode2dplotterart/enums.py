b9e0c0cac8c8a764fc00e7d7afda98bf84d0b853
Travis Bumgarner <travis.bumgarner@gmail.com>
2023-11-12 14:04:35 -0500

    PR cleanup

--- gcode2dplotterart/enums.py
+++ gcode2dplotterart/enums.py
@@ -7,37 +7,6 @@ class HandleOutOfBoundsEnum(Enum):
     Silent = "Silent"
 
 
-# class SpecialInstructionEnum(Enum):
-#     """
-#     Enum representing special instructions that can be included in G-code
-#     programs.
-
-#     Attributes:
-#       navigation_mode : str
-#         Command to separate the drawing instrument from the drawing surface.
-#         Useful to call before moving the print head when you don't want to
-#         draw.
-#       drawing_mode : str
-#         Command to connect the drawing instrument to the drawing surface.
-#         Useful to call before beginning to draw.
-#       pause : str
-#         Command to pause the program execution.
-#       program_end : str
-#         Command to indicate the end of the program.
-#       units_mm : str
-#         Command to set the units of measurement to millimeters.
-#       units_inches : str
-#         Command to set the units of measurement to inches.
-#     """
-
-#     navigation_mode = "navigation_mode"
-#     drawing_mode = "drawing_mode"
-#     pause = "pause"
-#     program_end = "program_end"
-#     units_mm = "units_mm"
-#     units_inches = "units_inches"
-
-
 class UnitsEnum(Enum):
     """
     Enum representing the types of units that are supported.
