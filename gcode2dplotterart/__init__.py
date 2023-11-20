f150f7a7674225fec8047c892e216629a542478a
Travis Bumgarner <travis.bumgarner@gmail.com>
2023-11-19 21:54:22 -0700

    Deploy website - based on f338570e29cdaa00d02df34d7d00a5594b47f57b

--- gcode2dplotterart/__init__.py
+++
@@ -1,3 +0,0 @@
-from .Plotter import Plotter2D, Plotter3D
-
-__all__ = ["Plotter2D", "Plotter3D"]
--- gcode2dplotterart/types.py
+++
@@ -1,5 +0,0 @@
-from typing import Union, Literal
-
-THandleOutOfBounds = Union[Literal["Warning"], Literal["Error"], Literal["Silent"]]
-
-TInstructionType = Union[Literal["setup"], Literal["plotting"], Literal["teardown"]]
