7498ce91a90abc0ad456264a1f61527870505fff
Travis Bumgarner <travis.bumgarner@gmail.com>
2025-02-14 14:40:24 -0600

    implemented (#52)

--- gcode2dplotterart-website/docusaurus.config.js
+++ gcode2dplotterart-website/docusaurus.config.js
@@ -52,6 +52,10 @@ const config = {
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
         },
+        gtag: {
+          trackingID: "G-HH63RN8FHH",
+          anonymizeIP: true,
+        },
       }),
     ],
   ],
